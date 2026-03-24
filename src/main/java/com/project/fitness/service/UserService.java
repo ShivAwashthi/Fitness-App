426d7ea65f1954ebe2750f14c9dfa1cbe3399d88
Shiv Awashthi <awasthishiv2002@gmail.com>
2026-03-24 21:28:34 +0530

    Authentication and Authorization (Security)

--- src/main/java/com/project/fitness/service/UserService.java
+++ src/main/java/com/project/fitness/service/UserService.java
@@ -3,8 +3,10 @@ package com.project.fitness.service;
 import com.project.fitness.dto.RegisterRequest;
 import com.project.fitness.dto.UserResponse;
 import com.project.fitness.model.User;
+import com.project.fitness.model.UserRole;
 import com.project.fitness.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 
@@ -13,13 +15,16 @@ import org.springframework.stereotype.Service;
 public class UserService {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     public UserResponse register(RegisterRequest request) {
+        UserRole role=request.getRole()!=null?request.getRole(): UserRole.USER;
         User user = User.builder()
                 .email(request.getEmail())
-                .password(request.getPassword())
+                .password(passwordEncoder.encode(request.getPassword()))
                 .firstName(request.getFirstName())
                 .lastName(request.getLastName())
+                .role(role)
         .build();
 
 //        User user = new User(null,
@@ -41,7 +46,7 @@ public class UserService {
         return mapToResponse(savedUser);
     }
 
-    private UserResponse mapToResponse(User savedUser) {
+    public UserResponse mapToResponse(User savedUser) {
         UserResponse response=new UserResponse();
         response.setId(savedUser.getId());
         response.setEmail(savedUser.getEmail());
